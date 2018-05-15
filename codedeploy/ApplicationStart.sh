e3049cc5543ae7780d1fe6539cca8005bd13d434
Plamen Mihaylov <Plamen_Mihaylov@epam.com>
2018-05-15 15:03:54 +0300

    Final prairs

--- codedeploy/ApplicationStart.sh
+++ codedeploy/ApplicationStart.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-#/opt/golang-questionnaire/questionnaireApp > /dev/null 2> /dev/null < /dev/null &
-
-/opt/golang-questionnaire/questionnaireApp &
-
-ps aux
-
-exit 1
+cd /opt/golang-questionnaire
+./questionnaireApp > /dev/null 2> /dev/null < /dev/null &
