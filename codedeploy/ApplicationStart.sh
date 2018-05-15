f4735864117238009b999bdd909b97062647aef5
Plamen Mihaylov <Plamen_Mihaylov@epam.com>
2018-05-15 11:49:55 +0300

    Change application name

--- codedeploy/ApplicationStart.sh
+++ codedeploy/ApplicationStart.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/opt/app > /dev/null 2> /dev/null < /dev/null &
+/opt/questionnaireApp > /dev/null 2> /dev/null < /dev/null &
--- codedeploy/ApplicationStop.sh
+++ codedeploy/ApplicationStop.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-killall app
+killall questionnaireApp
 exit 0
